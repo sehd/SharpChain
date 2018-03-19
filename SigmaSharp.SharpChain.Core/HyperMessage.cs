c56ef0f31c251f1d5e0af4f79a8cf18e157bbbc2
Emad Hosseini <EmadHosseini@hotmail.com>
2018-03-19 13:58:24 +0330

    -

--- SigmaSharp.SharpChain.Core/HyperMessage.cs
+++ SigmaSharp.SharpChain.Core/HyperMessage.cs
@@ -4,5 +4,8 @@ namespace SigmaSharp.SharpChain.Core
 {
     public class HyperMessage
     {
+        public User Composer { get; set; }
+        public User Recipient { get; set; }
+
     }
 }
---
+++ SigmaSharp.SharpChain.Core/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigmaSharp.SharpChain.Core
+{
+    public class User
+    {
+    }
+}
