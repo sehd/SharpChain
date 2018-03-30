be967d4d5c6d0817acc888929cd69b2b80746f25
Emad Hosseini <EmadHosseini@hotmail.com>
2018-03-30 06:34:16 +0430

    Simple test added

---
+++ SigmaSharp.SharpChain.Tests/BasicChainTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaSharp.SharpChain.Tests
+{
+    [TestClass]
+    public class BasicChainTests
+    {
+        [TestMethod]
+        public void BasinSingleChainAddAndRestore()
+        {
+            var chain = ChainFactory.Create<string>();
+            var sampleBlock = new List<string>()
+            {
+                "B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11"
+            };
+            foreach (var item in sampleBlock)
+            {
+                chain.AddBlock(item, chain.CreateBlock(item));
+            }
+            var res = chain.ViewChain().Select((obj, i) => new { content = obj, index = i });
+            foreach (var item in res)
+            {
+                Assert.AreEqual(item.content, sampleBlock[item.index]);
+            }
+        }
+    }
+}
